c0a53bb5de5d5c9ec2f7c06f813a3fd068cc9a34
Yaxin Luo <s215161@dtu.dk>
2024-06-17 09:29:16 +0200

    update codes

--- web_interface/connect_internet.py
+++
@@ -1,8 +0,0 @@
-import network
-# turn off the WiFi Access Point
-ap_if = network.WLAN(network.AP_IF)
-ap_if.active(False)
-# connect the device to the WiFi network
-wifi = network.WLAN(network.STA_IF)
-wifi.active(True)
-wifi.connect("WIFI_SSID", "WIFI_PASSWORD")
